59e26e1c00b16931d48336ff62736f2cb43e7832
Abbati Revanth Reddy <reddyrevanth235@gmail.com>
2024-09-14 14:56:59 +0530

    first commit
    uploaded files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Search Bar</h1>
+      <SearchBar />
+    </div>
+  );
+}
+
+export default App;
